cc5bcb34d551ce33d6b1cd134bd81b0661d02e82
fschwiet <fschwiet@gmail.com>
2014-05-29 17:04:12 -0700

    trying to write a run script

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 nothing to see here
+
+
+Requirements:
+  PM2  (npm install -g pm2)
+  user account "wwwuser" which can run the sites
--- run.sh
+++ run.sh
@@ -1,2 +1,32 @@
 #!/bin/sh
 
+for directory in ./sites/*; do
+ 
+	siteName=$(basename $directory)
+
+  	if [ -d "$directory" ]; then
+    	echo $siteName
+
+        # how will directory switching happen?
+
+    	# to detect if runnning, pipe result of pm2 prettyList to a node script.
+
+
+    	if [ $(ps -e -o cmd | grep pm2 | grep $siteName | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+		then
+		echo "lol"
+			cd ./sites/$siteName/src; 
+			pwd;
+			pm2 start ./run.sh --name $siteName --user wwwuser --interpreter bash
+		fi
+  	fi
+done
+
+
+# note:  limited environment variables available within this crontab script.
+#   In particular, $PATH is not set.
+
+#if [ $(ps -e -o cmd | grep pm2 | grep hello-server | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+#then
+#	cd /sites/www; /usr/local/bin/pm2 start hello-server.js --user wwwuser
+#fi
