28dc64fa56de986d82d73ced20439e2a10b0969b
Frank <fschwiet@gmail.com>
2014-06-01 17:42:16 -0700

    deploy script can check git status is empty

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,27 @@
 #!/bin/sh
 
+function is_git_empty {
+	
+	git_directory=$1
+
+	pushd  $git_directory > /dev/null
+
+	if [ -d .git ];
+	then
+		git_status=$(git status --porcelain) > /dev/null
+		popd > /dev/null
+
+		if [ ${git_status:+1} ];
+		then
+			echo;
+		else
+		  	echo "changes found in $git_directory"
+		fi
+	else
+		echo "git repository not found in $git_directory"
+	fi
+}
+
+
+echo "host: $(is_git_empty 'c:/src/cumulonimbus-host')"
+echo "host: $(is_git_empty 'c:/src/cumulonimbus-host/sites/cumulonimbus-project')"
