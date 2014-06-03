13f9a343b0af2fbc9b73d4c68d164c153f20d77e
Frank <fschwiet@gmail.com>
2014-06-02 18:22:11 -0700

    fixing boolean usage in bash script

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 function is_git_unclean {
+
+	git_directory=${1:?"is_git_unclean expected parameter"}
 	
-	git_directory=$1
-
 	pushd  $git_directory > /dev/null
-
+	
 	if [ -d .git ];
 	then
-		git_status=$(git status --porcelain) > /dev/null
+		change_count=$(git status --porcelain | wc -l)
 		popd > /dev/null
 
-		if [ ${git_status:+1} ];
+		if (($change_count > 0)) 
 		then
-			echo;
+			true
 		else
-		  	echo "changes found in $git_directory"
+			false
 		fi
 	else
-		echo "git repository not found in $git_directory"
+		true
 	fi
 }
 
+
 echo ${1:?"The first parameter indicating what site to deploy was missing."} > /dev/null
 
-if $(is_git_unclean .)
-then
+if is_git_unclean .
+else
 	echo "There are uncommitted changes in cumulonimbus-host."
 	exit
 fi
@@ -35,14 +36,24 @@ src_to_deploy=./sites/$1
 if ! [ -d $src_to_deploy ]
 then
 	echo "Unable to find site to deploy at $src_to_deploy"
+	exit 1
 fi
+
+if is_git_unclean $src_to_deploy
+then
+	echo "There are uncommitted changes in $src_to_deploy."
+	exit 1
+fi
+
 
 if ! [ -s $src_to_deploy/prerun.sh ]
 then
 	echo "Expected to find file $src_to_deploy/prerun.sh"
+	exit 1
 fi
 
 if ! [ -s $src_to_deploy/run.sh ]
 then
 	echo "Expected to find file $src_to_deploy/run.sh"
+	exit 1
 fi
