c281817a3c9139547bda2d3f1ee5ac75ba3c74c6
Frank <fschwiet@gmail.com>
2014-06-03 12:03:22 -0700

    copying deployment target to a folder

--- deploy.sh
+++ deploy.sh
@@ -23,15 +23,18 @@ function is_git_unclean {
 }
 
 
-echo ${1:?"The first parameter indicating what site to deploy was missing."} > /dev/null
+name_of_site=${1:?"The first parameter indicating what site to deploy was missing."}
+commit_to_deploy=${2:-"master"}
 
 if is_git_unclean .
 then
+	:
+else
 	echo "There are uncommitted changes in cumulonimbus-host."
 	exit
 fi
 
-src_to_deploy=./sites/$1
+src_to_deploy=./sites/$name_of_site
 
 if ! [ -d $src_to_deploy ]
 then
@@ -57,3 +60,21 @@ then
 	echo "Expected to find file $src_to_deploy/run.sh"
 	exit 1
 fi
+
+if ! [ -d ./deploys ] 
+then
+	mkdir ./deploys
+fi
+
+commit_id=$(git --git-dir $src_to_deploy/.git rev-parse master)
+dir_index=0
+
+while [ -d ./deploys/${commit_id}_${dir_index} ]
+do
+	((dir_index++))
+done
+
+deploy_target=./deploys/${commit_id}_${dir_index}
+
+mkdir $deploy_target
+git --git-dir $src_to_deploy/.git archive --format=tar $commit_to_deploy | tar --extract -C $deploy_target
