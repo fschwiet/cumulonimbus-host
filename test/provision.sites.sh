7fbb87a99e5f18cf0876706ae4c6f2f919d28497
Frank <fschwiet@gmail.com>
2014-06-12 15:12:38 -0700

    ahh learning unix I guess

--- test/provision.sites.sh
+++ test/provision.sites.sh
@@ -5,10 +5,7 @@ git clone /vagrant /cumulonimbus
 mkdir /cumulonimbus/sites
 sudo chown --recursive wwwuser:wwwuser /cumulonimbus
 
-
-TempCronFile=$(mktemp)
-(sudo crontab -l -u wwwuser ; echo '@reboot PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin bash -c "cd /cumulonimbus && ./run.sh >>~/cronrun_cumulonimbus 2>&1"') > $TempCronFile
-sudo crontab -u wwwuser $TempCronFile
+(sudo crontab -l -u wwwuser ; echo '@reboot PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin bash -c "cd /cumulonimbus && ./run.sh >>~/cronrun_cumulonimbus 2>&1"') | crontab -u wwwuser -
 
 echo "Next, run:"
 echo "    su wwwuser"
