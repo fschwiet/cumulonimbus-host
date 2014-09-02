bba7868c114633456885c957cda32b0260fbc740
Frank <fschwiet@gmail.com>
2014-09-02 14:49:02 -0700

    make link-config-folder.sh bail on any failure

--- scripts/link-config-folder.sh
+++ scripts/link-config-folder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 name_of_site=${1:?"The first parameter should indicate what site to deploy."}
 config_folder=${2:?"The second parameter should indicate what folder config files are copied from."}
 
